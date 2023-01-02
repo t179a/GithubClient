161cdfab2232423f7c95108f0469657b1b689e22
t179a <azmtzk179@gmail.com>
2023-01-03 05:23:11 +0900

    SearchApiを作成

---
+++ core/data/src/main/java/com/example/data/search/SearchApi.kt
@@ -0,0 +1,17 @@
+package com.example.data.search
+
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.toPersistentList
+
+class SearchApi(
+    private val networkService: SearchNetworkService
+) {
+    suspend fun searchRepository(word: String): PersistentList<RepositoryItem> {
+        return networkService.get<SearchResponse>(
+            url = "https://api.github.com/search/repositories",
+            query = word
+        ).RepositoryList.toPersistentList()
+    }
+}
+
+
