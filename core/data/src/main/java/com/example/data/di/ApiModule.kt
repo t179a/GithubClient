3051630147a1cc373b479ed3323617e82c6e14b1
t179a <azmtzk179@gmail.com>
2023-01-03 05:20:20 +0900

    ApiModuleを作成

---
+++ core/data/src/main/java/com/example/data/di/ApiModule.kt
@@ -0,0 +1,52 @@
+package com.example.data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class ApiModule {
+    @Provides
+    @Singleton
+    fun provideHttpClient(
+        okHttpClient: OkHttpClient
+    ): HttpClient {
+        val httpClient = HttpClient(OkHttp) {
+            engine {
+                config {
+                    preconfigured = okHttpClient
+                }
+            }
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        encodeDefaults = true
+                        isLenient = true
+                        allowSpecialFloatingPointValues = true
+                        allowStructuredMapKeys = true
+                        prettyPrint = false
+                        useArrayPolymorphism = false
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+        }
+        return httpClient
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        return builder.build()
+    }
+}
