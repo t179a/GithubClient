47cf83d6fff988bb1ccebce3d25c7b1dde61a927
t179a <azmtzk179@gmail.com>
2023-01-03 05:17:31 +0900

    SearchResponse,RepositoryItem,Owner data classを作成

---
+++ core/data/src/main/java/com/example/data/search/SearchResponse.kt
@@ -0,0 +1,32 @@
+package com.example.data.search
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class SearchResponse(
+    val RepositoryList: List<RepositoryItem>
+)
+
+@Serializable
+data class RepositoryItem(
+    @SerialName("full_name")
+    val name: String,
+    val owner: Owner,
+    val language: String?,
+    @SerialName("stargazers_count")
+    val stargazersCount: Long,
+    @SerialName("watchers_count")
+    val watchersCount: Long,
+    @SerialName("forks_count")
+    val forksCount: Long,
+    @SerialName("open_issues_count")
+    val openIssuesCount: Long
+)
+
+@Serializable
+data class Owner(
+    @SerialName("avatar_url")
+    val avatarUrl: String
+)
