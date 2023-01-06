e42b48aef0bf7bea47ea6ddc96527fa75c3a71ab
t179a <azmtzk179@gmail.com>
2023-01-07 08:48:06 +0900

    SearchViewModelを追加

---
+++ feature/search/src/main/java/com/example/search/SearchViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.search
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.data.search.RepositoryItem
+import com.example.data.search.SearchRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val searchRepository: SearchRepository
+) : ViewModel() {
+    private val _searchUiState: MutableStateFlow<SearchUiState> =
+        MutableStateFlow(SearchUiState())
+
+    val searchUiState: StateFlow<SearchUiState> = _searchUiState
+
+    fun onSearchRepository(word: String) {
+        viewModelScope.launch {
+           _searchUiState.update {
+               it.copy(loading = true)
+           }
+            searchRepository.search(word).catch {
+                _searchUiState.update {
+                    it.copy(loading = false, error = true)
+                }
+            }.collect {repositoryList ->
+                _searchUiState.update {searchUiState ->
+                    searchUiState.copy(loading = false, repositoryList = repositoryList)
+                }
+            }
+        }
+    }
+
+    fun onUpdateSearchWord(searchWord: String) {
+        _searchUiState.update {
+            it.copy(searchWord = searchWord)
+        }
+    }
+}
+
+
+data class SearchUiState(
+    val loading: Boolean = false,
+    val error: Boolean = false,
+    val repositoryList: List<RepositoryItem> = emptyList(),
+    val searchWord: String = ""
+)
