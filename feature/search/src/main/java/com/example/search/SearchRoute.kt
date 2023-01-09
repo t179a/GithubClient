2f2384e2f23a6826a34900122e7edd26a75baaff
t179a <azmtzk179@gmail.com>
2023-01-10 08:00:56 +0900

    modifierとuiStateを追加

--- feature/search/src/main/java/com/example/search/SearchRoute.kt
+++ feature/search/src/main/java/com/example/search/SearchRoute.kt
@@ -1,11 +1,16 @@
 package com.example.search
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun SearchRoute(
+    modifier: Modifier = Modifier,
     viewModel: SearchViewModel = hiltViewModel()
-){
-    SearchScreen(onSearch = { /*TODO*/ })
+) {
+    val uiState  by viewModel.searchUiState.collectAsState()
+    SearchScreen(modifier = modifier, onSearch = { viewModel.onSearchRepository(it) }, onWordChange = {viewModel.onUpdateSearchWord(it)}, uiState = uiState)
 }
