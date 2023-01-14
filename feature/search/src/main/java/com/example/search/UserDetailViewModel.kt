7b7eabcf406a7435dd7912ba2e8eb936098f887b
t179a <azmtzk179@gmail.com>
2023-01-14 20:49:38 +0900

    moleculeでstateを管理するように変更

--- feature/search/src/main/java/com/example/search/UserDetailViewModel.kt
+++ feature/search/src/main/java/com/example/search/UserDetailViewModel.kt
@@ -1,50 +1,57 @@
 package com.example.search
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import app.cash.molecule.AndroidUiDispatcher
+import app.cash.molecule.RecompositionClock
+import app.cash.molecule.launchMolecule
 import com.example.data.search.UserDetailRepository
 import com.example.data.search.UserItem
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class UserDetailViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val userDetailRepository: UserDetailRepository
+    userDetailRepository: UserDetailRepository
 ) : ViewModel() {
     private val userName: String = requireNotNull(savedStateHandle.get<String>("userName"))
+    private val followers = userDetailRepository.getFollowers(userName)
+    private val following = userDetailRepository.getFollowing(userName)
+    private val moleculeScope =
+        CoroutineScope(viewModelScope.coroutineContext + AndroidUiDispatcher.Main)
+    val userDetailUiState: StateFlow<UserDetailUiState> =
+        moleculeScope.launchMolecule(clock = RecompositionClock.ContextClock) {
+            userDetailPresenter(followingFlow = following, followersFlow = followers)
+        }
 
-    //TODO followingのflowとfollowersのflowを一つにまとめてuserDetailStateを更新する
-
-    private val _userDetailState: MutableStateFlow<UserDetailUiState> =
-        MutableStateFlow(UserDetailUiState(isLoading = true, isError = false))
-    val userDetailState = _userDetailState
-
-    init {
-        updateUserDetailState()
-    }
-
-    private fun updateUserDetailState() {
-        viewModelScope.launch {
-            _userDetailState.update {
-                it.copy(isLoading = true)
-            }
-            userDetailRepository.getFollowers(userName).catch {
-                _userDetailState.update {
-                    it.copy(isLoading = false, isError = true)
-                }
-            }.collect { userList ->
-                _userDetailState.update { userDatailUiState ->
-                    userDatailUiState.copy(isLoading = false, followingList = userList)
-                }
-            }
+    @Composable
+    fun userDetailPresenter(
+        followingFlow: Flow<PersistentList<UserItem>>,
+        followersFlow: Flow<PersistentList<UserItem>>
+    ): UserDetailUiState {
+        val followingList by followingFlow.collectAsState(initial = null)
+        val followersList by followersFlow.collectAsState(initial = null)
+        return if (followersList == null || followingList == null) {
+            UserDetailUiState(isLoading = true, isError = false)
+        } else {
+            UserDetailUiState(
+                isLoading = false,
+                isError = false,
+                followingList = followingList!!,
+                followersList = followersList!!
+            )
         }
     }
 }
-
 
 data class UserDetailUiState(
     val isLoading: Boolean = false,
