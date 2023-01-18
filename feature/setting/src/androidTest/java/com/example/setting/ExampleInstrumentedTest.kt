ab36d5c9a9c7d433321bb50aff2dfbfa5f97fa6e
t179a <azmtzk179@gmail.com>
2023-01-18 21:58:41 +0900

    login -> settingにモジュール名を変更

--- feature/setting/src/androidTest/java/com/example/setting/ExampleInstrumentedTest.kt
+++ feature/setting/src/androidTest/java/com/example/setting/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.login
+package com.example.setting
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- feature/setting/src/test/java/com/example/setting/ExampleUnitTest.kt
+++ feature/setting/src/test/java/com/example/setting/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.login
+package com.example.setting
 
 import org.junit.Test
 
