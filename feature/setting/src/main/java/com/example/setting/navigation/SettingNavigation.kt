3c74c1c33c7fcd3df299eed7473cbd95c7a54e43
t179a <azmtzk179@gmail.com>
2023-01-24 07:38:47 +0900

    AccountScreen,ProgressScreenのnavigationを設定

--- feature/setting/src/main/java/com/example/setting/navigation/SettingNavigation.kt
+++ feature/setting/src/main/java/com/example/setting/navigation/SettingNavigation.kt
@@ -1,19 +1,45 @@
 package com.example.setting.navigation
 
+import android.content.Context
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.example.setting.ui.LoginScreen
+import androidx.navigation.navDeepLink
+import com.example.setting.ui.account.AccountScreen
+import com.example.setting.ui.login.LoginScreen
+import com.example.setting.ui.progress.ProgressScreen
 
-const val settingNavigationRoute = "setting_route"
-fun NavController.navigateToAccount() {
-    this.navigate(settingNavigationRoute)
+private const val loginNavigationRoute = "setting_route"
+private const val progressNavigationRoute = "progress_route"
+private const val accountNavigationRoute = "account_route"
+private const val uri = "org.example.android.t179a://callback"
+fun NavController.navigateToAccount(context: Context) {
+    val pref = context.getSharedPreferences("githubSetting", Context.MODE_PRIVATE)
+    val accessToken = pref.getString("accessToken", "")
+    if(accessToken!!.isEmpty()){
+        this.navigate(loginNavigationRoute)
+    }else{
+        this.navigate(accountNavigationRoute)
+    }
+
 }
 
 fun NavGraphBuilder.settingScreen(
+    navigateToLoginScreen: () -> Unit,
+    navigateToAccountScreen: () -> Unit
 ) {
-    composable(route = settingNavigationRoute) {
-        LoginScreen(modifier = Modifier)
+    composable(route = accountNavigationRoute)  {
+        AccountScreen(navigateToLoginScreen = navigateToLoginScreen)
+    }
+    composable(route = loginNavigationRoute) {
+        LoginScreen(modifier = Modifier.fillMaxSize())
+    }
+    composable(
+        route = progressNavigationRoute,
+        deepLinks = listOf(navDeepLink { uriPattern = uri })
+    ) {
+        ProgressScreen(modifier = Modifier.fillMaxSize(), navigateToAccountScreen = navigateToAccountScreen)
     }
 }
