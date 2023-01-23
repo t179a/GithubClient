a172aefcf4861aed6dfc70b7c69ffbe9a97097f7
t179a <azmtzk179@gmail.com>
2023-01-24 07:39:42 +0900

    settingScreenのparameterを変更

--- app/src/main/java/com/example/githubclient/navigation/GithubClientNavHost.kt
+++ app/src/main/java/com/example/githubclient/navigation/GithubClientNavHost.kt
@@ -2,11 +2,13 @@ package com.example.githubclient.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.example.search.navigation.navigateToDetail
 import com.example.search.navigation.searchNavigationRoute
 import com.example.search.navigation.searchScreen
+import com.example.setting.navigation.navigateToAccount
 import com.example.setting.navigation.settingScreen
 
 @Composable
@@ -15,8 +17,9 @@ fun GithubClientNavHost(
     modifier: Modifier = Modifier,
     startDestination: String = searchNavigationRoute
 ) {
+    val context = LocalContext.current
     NavHost(navController = navController, startDestination = startDestination, modifier = modifier) {
         searchScreen(onUserRowClick = { userName -> navController.navigateToDetail(userName) }, onUserCardClick = {userName -> navController.navigateToDetail(userName = userName )})
-        settingScreen()
+        settingScreen(navigateToLoginScreen = {navController.navigateToAccount(context)}, navigateToAccountScreen = {navController.navigateToAccount(context)})
     }
 }
