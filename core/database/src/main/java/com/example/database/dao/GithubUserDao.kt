b15204a196b4b1ca6322311dc2d2739ee7d91de7
t179a <azmtzk179@gmail.com>
2023-01-28 13:50:28 +0900

    getAllUserの返り値を変更

--- core/database/src/main/java/com/example/database/dao/GithubUserDao.kt
+++ core/database/src/main/java/com/example/database/dao/GithubUserDao.kt
@@ -6,11 +6,12 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.database.model.GithubUser
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface GithubUserDao {
     @Query("SELECT * FROM githubUser")
-    fun getAllUser(): List<GithubUser>
+    fun getAllUser(): Flow<List<GithubUser>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertUser(user: GithubUser)
