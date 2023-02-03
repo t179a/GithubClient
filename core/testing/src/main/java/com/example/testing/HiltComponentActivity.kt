218767fb949ed1c373db983ea82cb65194d3e0ef
t179a <azmtzk179@gmail.com>
2023-02-03 18:47:59 +0900

    HiltComponentActivityを作成

---
+++ core/testing/src/main/java/com/example/testing/HiltComponentActivity.kt
@@ -0,0 +1,8 @@
+package com.example.testing
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltComponentActivity: ComponentActivity() {
+}
