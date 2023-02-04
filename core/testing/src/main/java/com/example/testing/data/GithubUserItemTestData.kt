4c38a25e35e6161f5b32836cefd61abc1290462c
t179a <azmtzk179@gmail.com>
2023-02-04 14:52:32 +0900

    githubUserTestDataを作成

---
+++ core/testing/src/main/java/com/example/testing/data/GithubUserItemTestData.kt
@@ -0,0 +1,16 @@
+package com.example.testing.data
+
+import com.example.data.search.GithubUserItem
+
+val githubUserItemTestData: List<GithubUserItem> =
+    List(20) {
+        GithubUserItem(
+            userName = "android",
+            userId = it.toLong(),
+            userUrl = "https://api.github.com/users/t179a",
+            avatarUrl = "https://avatars.githubusercontent.com/u/1024025?v=4",
+            followersUrl = "https://api.github.com/users/t179a/followers",
+            followingUrl = "https://api.github.com/users/t179a/following{/other_user}",
+            repositoryUrl = "https://api.github.com/users/t179a/repos",
+        )
+    }
