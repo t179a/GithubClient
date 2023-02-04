0676cc9b6566ff115f7dd41520211d1133913dea
t179a <azmtzk179@gmail.com>
2023-02-04 14:53:14 +0900

    textField_is_displayed testを追加,TODOを追加

--- feature/search/src/androidTest/java/com/example/search/SearchScreenTest.kt
+++ feature/search/src/androidTest/java/com/example/search/SearchScreenTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import com.example.search.ui.search.SearchScreen
 import com.example.search.ui.search.SearchUiState
+import com.example.testing.data.githubUserItemTestData
 import org.junit.Rule
 import org.junit.Test
 
@@ -24,5 +25,34 @@ class SearchScreenTest {
             )
         }
         composeTestRule.onNodeWithTag("circular_progress_indicator").assertIsDisplayed()
+    }
+
+    //TODO AsyncImageがある時のテストの書き方を調べる
+//    @Test
+//    fun githubUserRow_whenUiStateIsSuccess_displayed() {
+//        composeTestRule.setContent {
+//            SearchScreen(
+//                onSearch = {},
+//                onWordChange = {},
+//                onClickForDetail = {},
+//                onClickForSave = {},
+//                uiState = SearchUiState(userList = githubUserItemTestData)
+//            )
+//        }
+//        composeTestRule.onNodeWithTag("github_user_row").assertIsDisplayed()
+//    }
+
+    @Test
+    fun textField_is_displayed(){
+        composeTestRule.setContent {
+            SearchScreen(
+                onSearch = {},
+                onWordChange = {},
+                onClickForDetail = {},
+                onClickForSave = {},
+                uiState = SearchUiState()
+            )
+        }
+        composeTestRule.onNodeWithTag("text_field").assertIsDisplayed()
     }
 }
