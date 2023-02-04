4165fa1d02fc5851f683852e78464b5e0bdb8160
t179a <azmtzk179@gmail.com>
2023-02-04 18:54:00 +0900

    avatarUrlを変更

--- core/testing/src/main/java/com/example/testing/data/GithubUserItemTestData.kt
+++ core/testing/src/main/java/com/example/testing/data/GithubUserItemTestData.kt
@@ -8,7 +8,7 @@ val githubUserItemTestData: List<GithubUserItem> =
             userName = "android",
             userId = it.toLong(),
             userUrl = "https://api.github.com/users/t179a",
-            avatarUrl = "https://avatars.githubusercontent.com/u/1024025?v=4",
+            avatarUrl = "https://avatars.githubusercontent.com/u/67852630?v=4",
             followersUrl = "https://api.github.com/users/t179a/followers",
             followingUrl = "https://api.github.com/users/t179a/following{/other_user}",
             repositoryUrl = "https://api.github.com/users/t179a/repos",
